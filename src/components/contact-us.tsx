da57549e7529c6be451bceed4642a1b4cad8ac30
barham <baranali6688@gmail.com>
2024-02-09 13:07:35 +0300

    Done

--- src/components/contact-us.tsx
+++ src/components/contact-us.tsx
@@ -1,16 +1,44 @@
+"use client";
+import { sendEmail } from "@/actions";
 import AnimateSection from "./ui/animate-section";
-import { Button } from "./ui/button";
+import FormButton from "./ui/form-button";
 import Heading from "./ui/heading";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import { useFormState } from "react-dom";
+import { twMerge } from "tailwind-merge";
 
 export default function ContactUs() {
+  const [{ message, done }, formAction] = useFormState(sendEmail, {
+    message: "",
+    done: "yet",
+  });
+
   return (
-    <AnimateSection className="flex w-full flex-col gap-4" id="contact">
-      <Heading>Contact Us</Heading>
-      <Input placeholder="Email" />
-      <Textarea placeholder="Type Your Content Here" className="h-[400px]" />
-      <Button className=" rounded-full font-bold">Send Email</Button>
+    <AnimateSection id="contact" className="w-full">
+      <form action={formAction} className="flex w-full flex-col gap-4">
+        <Heading>Contact Us</Heading>
+
+        <Input placeholder="Email" type="email" name="senderEmail" />
+        <Textarea
+          placeholder="Type Your Content Here"
+          className="h-[400px]"
+          name="message"
+        />
+        <span
+          className={twMerge(
+            "rounded-full p-2 text-center text-sm",
+            done === "success" && " bg-green-100 text-green-900",
+            done === "yet" && " hidden",
+            done === "error" && "bg-red-100  text-red-600",
+          )}
+        >
+          {message}
+        </span>
+        <FormButton className=" rounded-full font-bold" type="submit">
+          Send Email
+        </FormButton>
+      </form>
     </AnimateSection>
   );
 }
