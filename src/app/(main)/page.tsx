cdff8e337bb04ebb728b75667670f5a511ce8b97
baran6688 <baranali6688@gmail.com>
2024-06-06 20:40:29 +0300

    py in the main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,8 +6,8 @@ import { TracingBeam } from "@/components/ui/tracing-beam";
 
 export default function Home() {
   return (
-    <main className=" ">
-      <TracingBeam childrenClassName="flex w-full space-y-20 flex-col items-center justify-between text-justify text-lg font-semibold md:pb-32 md:pt-10 lg:text-2xl">
+    <main className="">
+      <TracingBeam childrenClassName="flex w-full space-y-20 flex-col items-center justify-between text-justify text-lg font-semibold md:pb-32 py-12 lg:text-2xl">
         <Introduction />
         <About />
         <OurServices />
